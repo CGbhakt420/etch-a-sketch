7e7b198ee5e46df2a6682517679b166487e5be36
CGbhakt420 <sanchit.kr007@gmail.com>
2024-07-22 14:43:43 +0530

    Set up a hover effect so that the grid divs change color when the mouse passes over them and also added a color picker

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
 const GRIDSIZE = 600;
-let rows=16;
-let cols=16;
+let size=16;
 
 const sketchArea = document.querySelector('#sketcharea');
 sketchArea.style.width = `${GRIDSIZE}px`;
 sketchArea.style.height = `${GRIDSIZE}px`;
 
-function changeBgColor(){
-    
+// function setBgColor(){
+//     this.style.backgroundColor = "black";
+
+// }
+
+let color = "#000000"; 
+let click = true;
+
+function changeColor() {
+    color = document.getElementById('colorPicker').value;
 }
 
-function createcells(){
-    for(let i=0; i<rows*cols; i++){
+document.getElementById('colorPicker').addEventListener('input', function() {
+    changeColor();
+});
+
+// Function to handle mouseover event on grid cells
+function colorSquare() {
+    if (click) {
+        this.style.backgroundColor = color;
+    }
+}
+
+
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+      document.querySelector(".error").style.display = "none";
+      createcells(input);
+    } else {
+      document.querySelector(".error").style.display = "flex";
+    }
+  }
+
+function createcells(size){
+    let amount = size*size;
+    for(let i=0; i<amount; i++){
         const gridCell = document.createElement("div");
-        gridCell.style.width = `${(GRIDSIZE/cols)-2}px`;
-        gridCell.style.height = `${(GRIDSIZE/rows)-2}px`;
+        gridCell.style.width = `${(GRIDSIZE/size)-2}px`;
+        gridCell.style.height = `${(GRIDSIZE/size)-2}px`;
         gridCell.classList.add("cell");
 
         sketchArea.appendChild(gridCell);
 
-        gridCell.addEventListener("mouseover", changeBgColor);
+        gridCell.addEventListener("mouseover", colorSquare);
     }
 }
 
-createcells();
+createcells(16);
